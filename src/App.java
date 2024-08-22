671cd04edf9a70e3e7e2f80aec7c481e9c4518cc
Rayyan Saif <rayyansaif2008@gmail.com>
2024-08-21 19:06:15 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello there, World!");
+        int x = 0;
+        while (x != 10) {
+            x++;
+            System.out.println(x
+            );
+        }
+    }
+}
